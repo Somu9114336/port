c55c7e9d9b3ac6a778d24fcfcc0d07584aea6210
somu <sahoosomanatha2@gmail.com>
2024-03-10 10:51:55 +0530

    my project

--- login/src/App.js
+++ login/src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Signup from './Signup';
 import Login from './Login';
-import Home from "./Home";
-import About from './About';
-import Education from './Education';
-
+import Home from "./component/Home";
+import NavBar from "./component/NavBar";
+import About from "./component/About";
+import Portfolio from "./component/Portfolio";
+import Experience from './component/Experience';
+import Contact from './component/Contact'
 
 function App() {
   return (
@@ -16,8 +18,12 @@ function App() {
           <Route path='/' element={<Signup />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/home' element={<Home />}></Route>
-          <Route path='/about' component={<About />}></Route>
-          <Route path='/education' component={<Education />}></Route>
+          <Route path='/navbar' element={<NavBar />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/portfolio' element={<Portfolio />}></Route>
+          <Route path='/experience' element={<Experience />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+
 
 
         </Routes>
